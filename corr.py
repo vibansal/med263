7782f70352429ce733e85b974cc2b7180b9d3510
vibansal <vikas115@gmail.com>
2022-01-17 16:07:22 -0800

    updated corr.py to python3.6

--- corr.py
+++ corr.py
@@ -4,9 +4,12 @@ from scipy import stats
 
 ## code to calculate correlation coefficient between gene expression levels of two genes (user specified) 
 
-if len(sys.argv) < 3: print >>sys.stderr,"specify two gene names"; sys.exit(); 
+if len(sys.argv) < 3: 
+    print("specify two gene names",file=sys.stderr) 
+    sys.exit() 
 
 File = open('DATA/GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_median_rpkm.gct');
+if not File: print("gene expression file is missing"); sys.exit()
 gene1 = sys.argv[1]; gene2 = sys.argv[2]; 
 
 genenames = []; exps = [];
@@ -20,15 +23,14 @@ for line in File:
 	else:  genenames.append(gene[1]); exps.append([float(i) for i in gene[2:]]); 
 
 
-
 #print gene1,exp1; print gene2,exp2;
 
-if gene2 != "all": print gene1,gene2,stats.spearmanr(exp1,exp2)
+if gene2 != "all": print (gene1,gene2,stats.spearmanr(exp1,exp2))
 
 else: 
 
-	for i in xrange(len(genenames)):
+	for i in range(len(genenames)):
 		stat = stats.spearmanr(exp1,exps[i]); 
-		if stat[0] >= 0.90: print gene1,genenames[i],stat[0],stat[1]
+		if stat[0] >= 0.90: print(gene1,genenames[i],stat[0],stat[1])
 		#if stat.correlation >= 0.93: print gene1,genenames[i],stat.correlation,stat.pvalue
 
