0d754add74ec26eaaf14043ff159e6f3d7985f4b
vibansal <vikas115@gmail.com>
2022-01-17 16:13:18 -0800

    deleted phasing files

--- count_lof.py
+++ count_lof.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 import sys, os, random 
 
-if len(sys.argv) < 2: print >>sys.stderr,"specify VCF file name"; sys.exit(); 
+if len(sys.argv) < 2: print("specify VCF file name",file=sys.stderr); sys.exit(); 
 
 File  = open(sys.argv[1]);
 
@@ -9,13 +9,14 @@ File  = open(sys.argv[1]);
 n=0; combined_AC = 0; combined_AN =0; 
 
 for line in File: 
-	if line[0] == '#': continue;
-	var =line.strip().split('\t');
-	if var[6] != 'PASS': continue; 
-	alleles = var[4].split(',');
-	AC1 = var[7].split(';')[0][3:]; AC = AC1.split(',');
+	if line[0] == '#': continue
+	var =line.strip().split('\t')
+	if var[6] != 'PASS': continue 
+	alleles = var[4].split(',')
+	AC1 = var[7].split(';')[0][3:]
+        AC = AC1.split(',')
 	
-	for i in xrange(len(alleles)):
+	for i in range(len(alleles)):
 		a = alleles[i]; 
 		delta = len(a) -len(var[3]); 
 		if ',' + a +'|stop_gained' in var[7]: 
@@ -33,4 +34,4 @@ for line in File:
 
 File.close();
 
-print >>sys.stderr, "LoF variants:",n,"combined_AC:",combined_AC;
+print("LoF variants:",n,"combined_AC:",combined_AC,file=sys.stderr)
--- installation.sh
+++ installation.sh
@@ -2,20 +2,11 @@
 
 #2. bedtools
 
-#3. IGV 
-wget http://data.broadinstitute.org/igv/projects/downloads/IGV_2.3.89.zip
-unzip IGV_2.3.89.zip
-#java -jar IGV_2.3.89/igv.jar 
-
-# 4. python scipy: https://www.scipy.org/install.html 
+# 3. python scipy: https://www.scipy.org/install.html 
 sudo apt-get install python-numpy python-scipy 
 
-# 5. tabix 
+# 4. tabix 
 git clone https://github.com/samtools/htslib
 cd htslib;
 make
 
-#6. hapcut
-git clone https://github.com/vibansal/hapcut.git
-cd hapcut;
-make all
