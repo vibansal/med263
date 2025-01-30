b99124ba9701a99087ad228a5ce14fac6050c2e2
Bansal Lab <vibansal@users.noreply.github.com>
2025-01-30 10:19:26 -0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,11 +1,1 @@
-
-#0 python3.6 or higher
-
-# 1. python scipy: https://www.scipy.org/install.html 
-sudo apt-get install python-numpy python-scipy 
-
-# 2. tabix 
-git clone https://github.com/samtools/htslib
-cd htslib;
-make
-
+pip install numpy pandas matplotlib seaborn
